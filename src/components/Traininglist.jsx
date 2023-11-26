d1b4d74bc46147f742266e99793b926d2b34c8b7
ArttuuS <arttu.saily@gmail.com>
2023-11-26 20:54:49 +0200

    added customer info to training tab and parsed the date to right form

--- src/components/Traininglist.jsx
+++ src/components/Traininglist.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { AgGridReact } from "ag-grid-react";
+import dayjs from "dayjs";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
@@ -8,12 +9,18 @@ function Traininglist() {
   const [trainings, setTrainings] = useState([]);
 
   const fetchTrainings = () => {
-    fetch("http://traineeapp.azurewebsites.net/api/trainings")
+    fetch("http://traineeapp.azurewebsites.net/gettrainings")
       .then((response) => {
         if (response.ok) return response.json();
-        else throw new Error("Error in fetch" + response.statusText);
+        else throw new Error("Error in fetch: " + response.statusText);
       })
-      .then((data) => setTrainings(data.content))
+      .then((data) => {
+        const formattedTrainings = data.map((training) => ({
+          ...training,
+          date: dayjs(training.date).format("DD.MM.YYYY HH:mm"),
+        }));
+        setTrainings(formattedTrainings);
+      })
       .catch((err) => console.error(err));
   };
 
@@ -25,6 +32,18 @@ function Traininglist() {
     { field: "activity", sortable: true, filter: true },
     { field: "date", sortable: true, filter: true },
     { field: "duration", sortable: true, filter: true },
+    {
+      field: "customer.firstname",
+      sortable: true,
+      filter: true,
+      headerName: "First Name",
+    },
+    {
+      field: "customer.lastname",
+      sortable: true,
+      filter: true,
+      headerName: "Last Name",
+    },
   ]);
 
   return (
